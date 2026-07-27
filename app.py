7ed43774dd4237dade606d5d2cbd550ba4370d4d
Charvee <167700605+charveeee@users.noreply.github.com>
2026-07-26 18:32:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import numpy as np
 import scipy.signal as signal 
+import torch
+import torch.nn as nn
+import torch.optim as optim 
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
+torch.manual_seed(42)
+
+def gen_data(num_samples=500, length =200): 
+  "Generate pairs of clean PPG waves and noisy equivalents" 
